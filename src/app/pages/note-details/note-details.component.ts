8ad0f3a2885a530a42047b48a9a6659a862238bd
Luca Gennaro <lucagennaro91@gmail.com>
2023-05-22 09:40:18 +0200

    adding note, cancel method

--- src/app/pages/note-details/note-details.component.ts
+++ src/app/pages/note-details/note-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Note } from 'src/app/shared/note.model';
 import { NotesService } from 'src/app/shared/notes.service';
 
@@ -12,7 +13,7 @@ export class NoteDetailsComponent implements OnInit{
 
   note: Note;
 
-  constructor(private notesService: NotesService) {}
+  constructor(private notesService: NotesService, private router: Router) {}
 
   ngOnInit(): void {
     this.note = new Note()
@@ -20,6 +21,10 @@ export class NoteDetailsComponent implements OnInit{
 
   onSubmit(form: NgForm) {
     this.notesService.add(form.value)
+    this.router.navigateByUrl('/')
+  }
 
+  onCancel() {
+    this.router.navigateByUrl('/')
   }
 }
